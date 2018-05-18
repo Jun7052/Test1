4dcc5667be9c0545bff0641df9057519aee9e907
smhrc23 <smhrc23@DESKTOP-PJS8TIJ>
2018-05-18 11:54:09 +0900

    바꿈바꿈

--- Test1/src/Management/StudentDTO.java
+++ Test1/src/Management/StudentDTO.java
@@ -1,5 +1,49 @@
 package Management;
 
 public class StudentDTO {
+	String name;
+	int age;
+	String tel;
+	String number;
+	
+	public StudentDTO(String name, int age, String tel, String number) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.tel = tel;
+		this.number = number;
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	
 }
